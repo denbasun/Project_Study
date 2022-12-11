dc88914b3a92a8d47e67d4645ed4a262b9b44272
Den <denbasxl@gmail.com>
2022-12-11 10:41:08 +0200

    First commit study folder

---
+++ js/script.js
@@ -0,0 +1,95 @@
+// "use strict";
+
+// let number = 4.6;
+// console.log(-4/0);
+
+// console.log("string" * 9);
+
+// const persone = "Alex";
+
+// const bool = true; 
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     IsMarried: false
+// };
+
+// console.log(obj.age);
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.png', 6, 'apple.png'];
+// console.log(arr[1]);
+
+// const arr = ["a", "b", "c",];
+
+// arr[10] = '323';
+// console.log(arr);
+
+// const arrObj = {
+//     a:'a',
+//     b: 'b',
+//     c: 'c'
+//     abc: {
+//         df: [{}]
+//         def: {
+
+//         }
+//     }
+
+// }
+// arrObj.d = 'd';
+
+// console.log(arrObj['d']);
+
+// alert ('Hello');
+
+// const result = confirm('are u 18?');
+
+// console.log(result);
+
+// const answer = +prompt('are u 18?', '');
+
+// console.log(answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt('Name?', '');
+// answers[1] = prompt('Old?', '');
+// answers[2] = prompt('Place?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const namee = 'Ivan';
+
+// alert(`привет, ${namee}`);
+
+console.log(4+ '5');
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(6 == "6");
+console.log(2 + 2 * 2 === "6");
+
+const isChecked = true,
+        isColse = false;
+
+console.log(!isChecked || isColse);
+
+
